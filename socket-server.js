7313f9f85f23a753b6344941069a540a60b8731e
Aditya-Shrivastava <39914834+Aditya-Shrivastava@users.noreply.github.com>
2021-06-23 13:34:01 +0530

    add socket-server and messaging events

--- socket-server.js
+++ socket-server.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-let userCount = 0;
-
-app.get('/', (req, res) => {
-	res.send(`No of users connected: ${userCount}`);
+const io = require('socket.io')(server, {
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST']
+	}
 });
 
 io.on('connection', (socket) => {
-	console.log('user connected');
-	userCount++;
-	socket.on('disconnect', () => {
-		userCount--;
+	socket.on('join-room', ({ roomId, username }) => {
+		socket.join(roomId);
+		socket.broadcast.to(roomId).emit('user-connected', username);
+
+		socket.on('send-message', newMessage => {
+			socket.broadcast.to(roomId).emit('receive-message', newMessage);
+		});
+	})
+
+	socket.on("disconnect", () => {
+		console.log('User Left :(');
+		// socket.broadcast.emit("callEnded");
 	});
+
+	// socket.on("callUser", (data) => )
 });
 
 server.listen(5001, () => {
